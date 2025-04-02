9cbecf9391853acf420f33511f892d4debbf5bd6
Andrzej Pultyn <01178581@pw.edu.pl>
2025-04-02 18:14:56 +0200

    get_next_char tested

--- src/source.py
+++ src/source.py
@@ -1,28 +1,36 @@
 class Source:
     def __init__(self, source):
         self._source = source
-        self._char = 'EOF'
-        self._col = 1
+        self._char = None
+        self._col = 0
         self._row = 1
+        self._EOF_found = False
+        self._new_line_found = False
 
     def get_next_char(self):
         char = self._source.read(1)
+        self._col += 1
+
+        if self._new_line_found:
+            self._col = 1
+            self._row += 1
+            self._new_line_found = False
 
         if char == '\r':
             next_char = self._source.read(1)
-            if next_char != 'n':
+            if next_char != '\n':
                 self._source.seek(self._source.tell() - 1)
             char = '\n'
 
         if char == '\n':
-            self._col = 1
-            self._row += 1
+            self._new_line_found = True
 
         elif not char:
             char = 'EOF'
-
-        else:
-            self._col += 1
+            if not self._EOF_found:
+                self._EOF_found = True
+            else:
+                self._col -= 1
 
         self._char = char
         return char
--- tests/test_source.py
+++ tests/test_source.py
@@ -9,18 +9,235 @@ def test_get_next_char():
 
     my_source.get_next_char()
     assert my_source._char == 'H'
+    assert my_source._row == 1
+    assert my_source._col == 1
 
     my_source.get_next_char()
     assert my_source._char == 'e'
+    assert my_source._row == 1
+    assert my_source._col == 2
 
     my_source.get_next_char()
     assert my_source._char == 'l'
+    assert my_source._row == 1
+    assert my_source._col == 3
 
     my_source.get_next_char()
     assert my_source._char == 'l'
+    assert my_source._row == 1
+    assert my_source._col == 4
 
     my_source.get_next_char()
     assert my_source._char == 'o'
+    assert my_source._row == 1
+    assert my_source._col == 5
 
     my_source.get_next_char()
     assert my_source._char == 'EOF'
+    assert my_source._row == 1
+    assert my_source._col == 6
+
+
+def test_get_next_char_empty():
+    source = io.StringIO("")
+    my_source = Source(source)
+
+    my_source.get_next_char()
+    assert my_source._char == 'EOF'
+    assert my_source._row == 1
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'EOF'
+    assert my_source._row == 1
+    assert my_source._col == 1
+
+
+def test_get_next_char_linux_newline():
+    source = io.StringIO("Hi\nMe\nHe")
+    my_source = Source(source)
+
+    my_source.get_next_char()
+    assert my_source._char == 'H'
+    assert my_source._row == 1
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'i'
+    assert my_source._row == 1
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == '\n'
+    assert my_source._row == 1
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == 'M'
+    assert my_source._row == 2
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'e'
+    assert my_source._row == 2
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == '\n'
+    assert my_source._row == 2
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == 'H'
+    assert my_source._row == 3
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'e'
+    assert my_source._row == 3
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == 'EOF'
+    assert my_source._row == 3
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == 'EOF'
+    assert my_source._row == 3
+    assert my_source._col == 3
+
+
+def test_get_next_char_windows_newline():
+    source = io.StringIO("Hi\r\nMe\r\nHe")
+    my_source = Source(source)
+
+    my_source.get_next_char()
+    assert my_source._char == 'H'
+    assert my_source._row == 1
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'i'
+    assert my_source._row == 1
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == '\n'
+    assert my_source._row == 1
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == 'M'
+    assert my_source._row == 2
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'e'
+    assert my_source._row == 2
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == '\n'
+    assert my_source._row == 2
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == 'H'
+    assert my_source._row == 3
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'e'
+    assert my_source._row == 3
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == 'EOF'
+    assert my_source._row == 3
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == 'EOF'
+    assert my_source._row == 3
+    assert my_source._col == 3
+
+
+def test_get_next_char_mixed():
+    source = io.StringIO("Hi\nMe\r\nHe\n\n\r\nHi")
+    my_source = Source(source)
+
+    my_source.get_next_char()
+    assert my_source._char == 'H'
+    assert my_source._row == 1
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'i'
+    assert my_source._row == 1
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == '\n'
+    assert my_source._row == 1
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == 'M'
+    assert my_source._row == 2
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'e'
+    assert my_source._row == 2
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == '\n'
+    assert my_source._row == 2
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == 'H'
+    assert my_source._row == 3
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'e'
+    assert my_source._row == 3
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == '\n'
+    assert my_source._row == 3
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == '\n'
+    assert my_source._row == 4
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == '\n'
+    assert my_source._row == 5
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'H'
+    assert my_source._row == 6
+    assert my_source._col == 1
+
+    my_source.get_next_char()
+    assert my_source._char == 'i'
+    assert my_source._row == 6
+    assert my_source._col == 2
+
+    my_source.get_next_char()
+    assert my_source._char == 'EOF'
+    assert my_source._row == 6
+    assert my_source._col == 3
+
+    my_source.get_next_char()
+    assert my_source._char == 'EOF'
+    assert my_source._row == 6
+    assert my_source._col == 3
