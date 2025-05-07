7129c004290c126cc4770b3a4f9a5b96acaeaaf1
Andrzej Pultyn <01178581@pw.edu.pl>
2025-05-07 08:48:03 +0200

    99% percent test coverage

--- src/parser/parser_objects.py
+++ src/parser/parser_objects.py
@@ -17,73 +17,13 @@ class ParserObject:
     def __post_init__(self):
         self._name = self.__class__.__name__
 
-    def accept(self, visitor: "Visitor"):
-        method = getattr(visitor, f"visit_{self._name}", None)
-        if method is None:
-            return visitor.generic_visit(self)
-        return method(self)
-
-
-class Visitor:
-    def generic_visit(self, node: ParserObject):
-        for attr in fields(node):
-            value = getattr(node, attr.name)
-            if isinstance(value, ParserObject):
-                value.accept(self)
-            if isinstance(value, list):
-                for item in value:
-                    if isinstance(item, ParserObject):
-                        item.accept(self)
-
-
-class PrintVisitor(Visitor):
-    def __init__(self, ident: int = 2, pos=False):
-        self._level = 0
-        self._ident = ident
-
-    def _pad(self):
-        return " " * (self._level * self._ident)
-
-    def generic_visit(self, node):
-        print(f"{self._pad()}{node._name}  pos={node.pos}")
-        self._level += 1
-        super().generic_visit(node)
-        self._level -= 1
-
-    def visit_Identifier(self, node: "Identifier"):
-        print(f"{self._pad()}Identifier  name={node.name}  pos={node.pos}")
-
-    def visit_IfStmt(self, node: "IfStmt"):
-        print(f"{self._pad()}IfStatement  pos={node.pos}")
-        self._level += 1
-        print(f"{self._pad()}condition:")
-        self._level += 1
-        print(node.condition.accept(self))
-        self._level -= 1
-        print(f"{self._pad()}body:")
-        self._level += 1
-        print(node.body.accept(self))
-        self._level -= 1
-        print(f"{self._pad()}elif statements:")
-        self._level += 1
-        for elifstmt in node.elif_statements:
-            print(elifstmt.accept(self))
-        self._level -= 1
-        print(f"{self._pad()}else statement:")
-        self._level += 1
-        if node.else_body:
-            print(node.else_body.accept(self))
-        self._level -= 2
-
 
 class Statement(ParserObject):
-    def __repr__(self):
-        return None
+    pass
 
 
 class Expression(ParserObject):
-    def __repr__(self):
-        return None
+    pass
 
 
 @dataclass
