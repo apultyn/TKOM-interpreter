4de446c0e8ed6cc6d8dc7fd6051a861ffce27c31
Andrzej Pultyn <01178581@pw.edu.pl>
2025-05-13 16:16:50 +0200

    psc command works

--- src/interpreter/util.py
+++ src/interpreter/util.py
@@ -9,8 +9,8 @@ from src.util.source import Source
 def open_source(path: Optional[Path]):
     if path is not None:
         with path.open("r", encoding="utf-8", newline="") as file_handle:
-            yield Source(file_handle)
+            yield file_handle
     else:
         if sys.stdin.isatty():
             raise SystemExit("No source code supplied - use -i <file> or pipe data in.")
-        yield Source(sys.stdin)
+        yield sys.stdin
